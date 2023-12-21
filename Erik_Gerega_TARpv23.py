30b2cb4ae0613469d558eb76e5aab2971e7a0a7f
opilane <opilane@HP-CZC2349HTR>
2023-12-21 12:35:09 +0200

    21.12.23

--- Erik_Gerega_TARpv23.py
+++ Erik_Gerega_TARpv23.py
@@ -1,1 +1,25 @@
-print("Tere maailm")
+#1
+#print("Tere maailm")
+#nimi=input("Mis on sinu nimi?")
+#vanus=int(intup("kui vana sa oled"))
+#("Tere tulemast "+nimi)
+#print("Tere tulemast ",nimi)," Sa oled" ,vanus, "aasta vana)"
+#print("Tere tulemast {0}. sa oled {1} aastat vana".format(nimi,vanus))
+#print("muutuja vanus=" ,vanus ,"tüüp on",type(vanus))
+#2
+#vanus = 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#kas_käib_koolis = True
+#print("muutuja eesnimi=" ,eesnimi ,"tüüp on",type(eesnimi))
+#print("muutuja pikkus=" ,pikkus ,"tüüp on",type(pikkus))
+#print("muutuja kas_käib_koolis=" ,kas_käib_koolis ,"tüüp on",type(kas_käib_koolis))
+#print("muutuja pikkus=" ,pikkus ,"tüüp on",type(pikkus))
+#3
+from random import *
+konfeti=randint(10,100)
+print("konfeti",konfeti)
+spizdil=int(input("skolko spizdit"))
+konfeti-=spizdil
+print("na stole teper",konfeti,"konfet")
+
