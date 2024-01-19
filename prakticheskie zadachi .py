54fb0a128a8a389b735448da5014b8a6f8420963
opilane <opilane@HP-CZC2349HTR>
2024-01-19 11:39:33 +0200

    19.01.2024

---
+++ prakticheskie zadachi .py
@@ -0,0 +1,61 @@
+##1
+
+#for i in range(0,15,1):
+#    arv=float(input("Sisesta {0}.arv".format(i)))
+#    if arv==int(arv)
+#        r+=1
+#print("Täisarvude arv on "+str(r))
+##while true
+#r=0
+#i=0
+#while True:
+#    1+=1
+#    arv=float(input("sisesta {0}.arv ".format(i)))
+#    if arv==int(arv):
+#        r+=1
+#    if 1==15: break
+#print("Täisarvude arv on "+str(r))
+
+##while tingimusega
+
+#r=0
+#i=0
+#while i<15:
+#    1+=1
+#    arv=float(input("sisesta {0}.arv ".format(i)))
+#    if arv==int(arv):
+#        r+=1
+
+#print("Täisarvude arv on "+str(r))
+
+
+#2
+c=1
+b=1
+a=int(input())
+for i in range(1,a,1):
+    b+=1
+    c+=b
+print(c)
+
+
+c=1
+b=1
+a=int(input())
+while True:
+    b+=1
+    c+=b
+    if b==a:
+        break
+print(c)
+
+c=1 
+b=1
+a=int(input())
+while a>b:
+    b+=1
+    c+=b
+print(c)
+
+
+
