c1f4b4ce97ab142e807a69fefdf13eb23addace9
Erik Gerega <154507933+tugosera@users.noreply.github.com>
2024-01-31 11:27:27 +0200

    Add files via upload

---
+++ Kontrolltöö_Kordused.py
@@ -0,0 +1,35 @@
+﻿#1
+
+
+#try:
+#    i=int(input("sisesta sõnaseadjate arv ")) 
+#except:
+#    ValueError
+
+
+#if 0>i>9:
+#    print("error")
+#else:
+#    for i in range (0,i,1):
+#        print(" -----  ")
+#        print("|  O  |  ")
+#        print("!  -  !  ")
+#        print(" -----  ")
+
+#2
+
+
+a=int(input("vvedite chislo"))
+d=int(input("vvedite chislo ese raz"))
+b=int(input("vvedite vtoroe chislo"))
+
+while True:
+    for a in range(0,d,1):
+        c=int()
+        c=a**b
+        if c<a**3:
+            print(c)
+        elif c>a**4:
+            break
+        
+
