d00764cbca560cffc7459a2174af74b2825426c3
Erik Gerega <154507933+tugosera@users.noreply.github.com>
2024-02-21 11:44:22 +0200

    Add files via upload

---
+++ MyModul.py
@@ -0,0 +1,31 @@
+﻿def Summa(arv1:int,arv2:int,arv3=0)->int:
+    """  Funktsioon tagastab kolme asrvu summa
+         Summa(arv1,arv2,arv3)
+
+
+    param int arv1: arv1 sisestab kasutaja
+    param int arv2: arv2 sisestab kasutaja
+    param int arv3: arv3 sisestab kasutaja
+
+    """
+
+    s=arv1+arv2+arv3
+    return s
+
+def arithmetic(arv1:int,arv2:int,sümbol:str) -> int:
+    if sümbol=="+":
+        s=arv1+arv2
+    elif sümbol=="-":
+        s=arv1-arv2
+    elif sümbol=="*":
+        s=arv1*arv2
+    elif sümbol=="/":
+        s=arv1/arv2
+    else:
+        s="Неизвестная операция"
+    return s
+
+
+
+
+
---
+++ sss.py
@@ -0,0 +1,13 @@
+﻿from MyModul import * #Summa as s
+#b=int(input("Sisesta arv2 "))
+#summa_3=Summa(3,b,int(input("Kolmas arv: ")))
+#summa_31=Summa(100,100)
+
+#print(summa_3)
+#print(summa_31)
+
+a=int(input("Sisesta arv1 "))
+b=int(input("Sisesta arv2 "))
+c=(input("Sisesta sümbol "))
+bubu=arithmetic(a,b,c)
+print(bubu)
