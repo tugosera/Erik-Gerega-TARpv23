21615aef628fc157415a2acea265535ff0c05d55
opilane <opilane@HP-CZC2349HTR>
2024-02-23 10:13:02 +0200

    23.02.2024

---
+++ dddd.py
@@ -0,0 +1,46 @@
+﻿from MyModul import * #Summa as s
+#b=int(input("Sisesta arv2 "))
+#summa_3=Summa(3,b,int(input("Kolmas arv: ")))
+#summa_31=Summa(100,100)
+
+#print(summa_3)
+#print(summa_31)
+
+a=int(input("Sisesta arv1 "))
+b=int(input("Sisesta arv2 "))
+c=(input("Sisesta sümbol "))
+bubu=arithmetic(a,b,c)
+print(bubu)
+
+#from modul import *
+
+#while True:
+#    try:
+#        aasta=int(input("Sisesta aastanumber: "))
+#        break
+#    except:
+#        print("viga")
+#a=isYearLeap(aasta)
+#print(a)
+
+#from modul import *
+
+while True:
+    try:
+        a=int(input("Sisesta külg: "))
+        break
+    except:
+        print("viga")
+S,P,d=square(a)
+print(f"S={S}, P={P}, d={d}")
+
+#from modul import *
+
+while True:
+    try:
+        kuu=int(input("Kuu number: "))
+        break
+    except:
+        print("Viga")
+a=season(kuu)
+print(a)
---
+++ modul.py
@@ -0,0 +1,55 @@
+﻿def arithmetic(arv1:int,arv2:int,sümbol:str) -> int:
+    if sümbol=="+":
+        s=arv1+arv2
+    elif sümbol=="-":
+        s=arv1-arv2
+    elif sümbol=="*":
+        s=arv1*arv2
+    elif sümbol=="/":
+        s=arv1/arv2
+    else:
+        s="Неизвестная операция"
+    return s
+
+def isYearLeap(aasta: int)->bool:
+    """Funktsioon otsustab kas aasta on liigaasta või ei ole.
+    Tagastab True kui aasta on liigaasta ja false kui on tavaline aasta.
+
+    parem int aasta: Aasta sissestab kasutaja
+    type: bool
+    """
+    if aasta%4==0 and aasta%100!=0:
+        return True
+    else:
+        return False 
+
+from math import *
+def square(külg:float)->any:
+    """
+    """
+    S=külg**2
+    P=4*külg
+    d=külg*sqrt(2)
+    return S,P,d
+
+from math imprt *
+def season(a:int)->str:
+    """
+    """
+    while True:
+        if a>0 and a<13:
+            break
+        else:
+            try:
+                a=int(input("Ainult 1-12, Sisesta veel kord number: "))
+            except:
+                print("Vigaandmetüübiga")
+    if a>2 and a<6:
+        s="kevad"
+    elif a>5 and a<9:
+        s="suvi"
+    elif a>8 and a<12:
+        s="kevad"
+    else:
+        s="talv"
+return s
